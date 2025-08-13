c7829c23a7163275fe1e5a909608eb356aaee3ec
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-08-14 01:36:10 +0530

    removed react dev tools package

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import "./index.css";
 import App from "./App.tsx";
-
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
