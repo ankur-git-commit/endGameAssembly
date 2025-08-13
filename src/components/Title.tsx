fe53c63e5fc32ad59e77eec9c5a84ef266da50b5
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-08-13 05:48:06 +0530

    completed -1

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,14 +3,14 @@ import NotificationBanner from "./NotificationBanner";
 function Title({ gameState, incorrectGuesses, pogLang }: any) {
     return (
         <div>
-            <div className="font-Han mb-16 flex flex-col items-center px-10 justify-between">
+            <div className="font-Han mb-16 flex flex-col items-center justify-between px-10">
                 <h1 className="text-3xl text-[#F9F4DA]">Assembly: Endgame</h1>
                 <div className="text-center text-[#8E8E8E]">
                     <p>Guess the word in under 8 attemps to keep the</p>
                     <p>programming world safe from Assembly!</p>
                 </div>
             </div>
-            <div className="font-Han flex w-full flex-col items-center justify-center h-24">
+            <div className="font-Han flex h-24 w-full flex-col items-center justify-center">
                 <NotificationBanner
                     gameState={gameState}
                     incorrectGuesses={incorrectGuesses}
